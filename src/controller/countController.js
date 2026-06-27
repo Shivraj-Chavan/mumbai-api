a55b29cfc927b69e07eba7ab34fbb9ae53e8f9c4
shivraj-chavan <shivrajchavan1902@gmail.com>
2026-06-27 11:32:11 +0000

    chnges adds

--- src/controller/countController.js
+++ src/controller/countController.js
@@ -3,11 +3,10 @@ import pool from "../config/db.js";
 export const trackVisit = async (req, res) => {
   try {
     await pool.query(`UPDATE website_visits SET count = count + 1 WHERE id = 1`);
-    const [rows] = await pool.query("SELECT COUNT(*) AS count FROM website_visits");
-    const visitCount = rows[0]?.count || 0;
-    res.json({ visitCount });
+    const [[row]] = await pool.query(`SELECT count FROM website_visits WHERE id = 1`);
+    console.log({row})
+    res.status(200).json({ visitCount: row.count });
   } catch (error) {
-    console.error("Failed to track visit:", error);
     res.status(500).json({ msg: "Failed to track visit", error: error.message });
   }
 };
